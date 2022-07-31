d8086f18265a97e13758a889ca83c752a2036b12
Danilo <dundeepessoal@gmail.com>
2022-07-30 21:50:15 -0300

    typo

--- seed.sql
+++ seed.sql
@@ -1,11 +1,11 @@
 CREATE TABLE candidatos(Nome VARCHAR(30), Tipo VARCHAR(30), Codigo INT, Partido VARCHAR(30), Foto VARCHAR(20), Vice_Nome VARCHAR(30), Vice_Partido VARCHAR(30), Vice_Foto VARCHAR(20), Votos INT);
 
 INSERT INTO candidatos VALUES("Christianne Varão","vereador",51222,"PEN","cv1.jpg","","","",0);
-INSERT INTO candidatos VALUES("Homero do Zé Filho","vereador",55555,"PSL","cv2.jpg","","",0);
-INSERT INTO candidatos VALUES("Dandor","vereador",43333,"PV","cv3.jpg","","",0);
-INSERT INTO candidatos VALUES("Filho","vereador",15123,"MDB","cv4.jpg","","",0);
-INSERT INTO candidatos VALUES("Joel Varão","vereador",27222,"PSDC","cv5.jpg","","",0);
-INSERT INTO candidatos VALUES("Professor Clebson Almeida","vereador",45000,"PSDB","cv6.jpg","","",0);
+INSERT INTO candidatos VALUES("Homero do Zé Filho","vereador",55555,"PSL","cv2.jpg","","","",0);
+INSERT INTO candidatos VALUES("Dandor","vereador",43333,"PV","cv3.jpg","","","",0);
+INSERT INTO candidatos VALUES("Filho","vereador",15123,"MDB","cv4.jpg","","","",0);
+INSERT INTO candidatos VALUES("Joel Varão","vereador",27222,"PSDC","cv5.jpg","","","",0);
+INSERT INTO candidatos VALUES("Professor Clebson Almeida","vereador",45000,"PSDB","cv6.jpg","","","",0);
 
 INSERT INTO candidatos VALUES("Chiquinho do Adbon","prefeito",12,"PDT","cp3.jpg","Arão","PRP", "v3.jpg",0);
 INSERT INTO candidatos VALUES("Malrinete Gralhada","prefeito",15,"MDB","cp2.jpg","Biga","MDB", "v2.jpg",0);
--- src/Database/DatabaseConnector.php
+++ src/Database/DatabaseConnector.php
@@ -3,36 +3,36 @@
  * Classe de conexão com o banco de dados
  */
 class DatabaseConnector {
-   
-   private $dbConnection;     /**< Conector do banco de dados */
 
-   /**
-    * Construtor da classe
-    * Cria a conexão no banco de dados usando parâmetros previamente estabelecidos. Em caso de erro, retorna a mensagem de erro.
-    */
-   public function __construct() {
-     $url = parse_url(getenv('CLEARDB_DATABASE_URL'))
-      $host = $url['host'];
-      $port = 3306;
-      $database = substr($url['path'],1);
-      $username = $url['user'];
-      $password = $url['pass'];
+  private $dbConnection;     /**< Conector do banco de dados */
 
-      try {
-         $this->dbConnection = new \PDO(
-            "mysql:host=$host;port=$port;charset=utf8mb4;dbname=$database",
-            $username,
-            $password
-         );
-      } catch (\PDOException $e) {
-         exit($e->getMessage());
-      }
-   }
+  /**
+   * Construtor da classe
+   * Cria a conexão no banco de dados usando parâmetros previamente estabelecidos. Em caso de erro, retorna a mensagem de erro.
+   */
+  public function __construct() {
+    $url = parse_url(getenv('CLEARDB_DATABASE_URL'));
+    $host = $url['host'];
+    $port = 3306;
+    $database = substr($url['path'],1);
+    $username = $url['user'];
+    $password = $url['pass'];
 
-   /**
-    * Getter para dbConnection
-    */
-   public function get_connection() {
-      return $this->dbConnection;
-   }
+    try {
+      $this->dbConnection = new \PDO(
+        "mysql:host=$host;port=$port;charset=utf8mb4;dbname=$database",
+        $username,
+        $password
+      );
+    } catch (\PDOException $e) {
+      exit($e->getMessage());
+    }
+  }
+
+  /**
+   * Getter para dbConnection
+   */
+  public function get_connection() {
+    return $this->dbConnection;
+  }
 }
